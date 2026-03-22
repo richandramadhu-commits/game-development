ec6e01464c1a7142cc60f718a515482847d496ad
richandramadhu-commits <richandra.madhu@gmail.com>
2026-03-22 13:36:40 +0100

    Add files via upload

--- aim.py
+++ aim.py
@@ -1,10 +1,13 @@
 import pgzrun
+import random 
+message=""
 WIDTH=999 
 HEIGHT=999
 gar=Actor("garfield")
 def draw():
     screen.fill("yellow")
     gar.draw()
+    screen.draw.text(message, (490,10),color="black")
 def update():
     if keyboard.a:
         gar.x-=10
@@ -14,4 +17,14 @@ def update():
         gar.y-=10
     if keyboard.s:
         gar.y+=10
+def G():
+    gar.x=random.randint(0,999)
+    gar.y=random.randint(0,999)
+def on_mouse_down(pos):
+    global message 
+    if gar.collidepoint(pos):
+        G()
+        message="nice shoot"
+    else:
+        message="you missed"
 pgzrun.go()
